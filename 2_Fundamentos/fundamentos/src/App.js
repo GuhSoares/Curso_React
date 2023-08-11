9566363803f46b60b8a46a713f6256c359c79532
Gustavo Soares <gustavo.silva@hens.com.br>
2023-08-11 12:46:34 -0300

    Importando Componente

--- 2_Fundamentos/fundamentos/src/App.js
+++ 2_Fundamentos/fundamentos/src/App.js
@@ -1,10 +1,14 @@
+// components
+import FirstComponent from './components/FirstComponent';
 
+// styles
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Fundamentos React</h1>
+      <FirstComponent />
     </div>
   );
 }
