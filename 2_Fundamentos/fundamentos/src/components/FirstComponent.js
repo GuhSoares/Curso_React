4604b833321464e3b436e6147d2d243398bdeed3
Gustavo Soares <gustavo.silva@hens.com.br>
2023-08-15 16:27:56 -0300

    Chore: hierarquia dos components

--- 2_Fundamentos/fundamentos/src/components/FirstComponent.js
+++ 2_Fundamentos/fundamentos/src/components/FirstComponent.js
@@ -1,8 +1,12 @@
+import MyComponent from "./MyComponent";
+
 const FirstComponent = () => {
 
     return (
         <div>
             <h1>First Component</h1>
+            <p className="teste">Meu Texto</p>
+            <MyComponent />
         </div>
     )
 }
