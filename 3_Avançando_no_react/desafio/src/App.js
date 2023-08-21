380f6d78d55a21b5e510326be0e3bf47ed904e04
Gustavo Soares <gustavo.silva@hens.com.br>
2023-08-21 16:53:06 -0300

    Chore: add img em src

--- 3_Avançando_no_react/desafio/src/App.js
+++ 3_Avançando_no_react/desafio/src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+
+import City from './assets/city.jpg';
 
 function App() {
   return (
     <div className="App">
       <h1>Avançando em React</h1>
+      {/* imagem em public */}
       <div>
         <img src="/img1.jpg" alt="Paisagem"/>
+        </div>
+        {/* imagem em assets */}
+        <div>
+          <img src={City} alt="Cidade"/>
         </div>
     </div>
   );
